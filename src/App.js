4f40e27a77c6e0231a65d3f0c3e84cc09dd46bca
agilles1 <adam.g.tpt@gmail.com>
2021-08-15 12:17:29 -0400

    Add NavBar Layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
 import ScoreContainer from './containers/ScoreContainer';
 import VideoContainer from './containers/VideoContainer';
-import { Container, Col, Row } from 'react-bootstrap'
+import { Container, Col, Row, Navbar, Nav } from 'react-bootstrap'
 
 
 function App() {
 
   return (
-    <Container className="App">
+    <Container fluid className="App">
       <Row>
-      <Col>
+        <Navbar>
+          <Container>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Shot List</Nav.Link>
+              <Nav.Link href="#pricing">MultiCam</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </Row>
+     <Row>
+      <Col> 
         <ScoreContainer />
       </Col>
-      <Col>
+     <Col xs={5}>
         <VideoContainer />
       </Col>
-      </Row>
+     </Row>
     </Container>
   );
 }
