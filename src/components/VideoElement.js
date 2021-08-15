99c2845d6a931826ab0bce802581303d0bc83bc2
agilles1 <adam.g.tpt@gmail.com>
2021-08-15 10:52:07 -0400

    Add Video Element placeholder

---
+++ src/components/VideoElement.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+import ReactPlayer from 'react-player'
+
+export default function VideoElement() {
+
+    return (
+        <ReactPlayer url="https://www.youtube.com/watch?v=C0DPdy98e4c"/>
+    )
+}
