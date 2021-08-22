4415b49d6f3e98b157e812f61f94949d46759d45
agilles1 <adam.g.tpt@gmail.com>
2021-08-22 08:19:59 -0400

    Stub out ShotContainer

--- src/containers/ShotContainer.js
+++ src/containers/ShotContainer.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react'
+import { Carousel } from 'react-bootstrap'
+import ShotElement from '../components/ShotElement'
 
-function ShotContainer() {   
 
-  return ( <></>);
+function ShotContainer({ pageNumber }) {   
 
+  return (
+    <Carousel activeIndex={pageNumber-1}  interval={null} indicators={false} controls={false}>
+      <Carousel.Item >
+        <ShotElement />
+        <ShotElement />
+        <ShotElement />
+        <ShotElement />
+        <ShotElement />
+        <ShotElement />
+      </Carousel.Item>
+      <Carousel.Item>
+        <ShotElement />
+      </Carousel.Item>
+    </Carousel>
+    
+    );
 }
 
 export default ShotContainer
