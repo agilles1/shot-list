a7707336f0fb463a6e3e6c0613907069656a9bfb
agilles1 <adam.g.tpt@gmail.com>
2021-08-22 08:19:14 -0400

    Create ShotElement

---
+++ src/components/ShotElement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+function ShotElement() {
+
+    return (
+        <Button variant="primary" >
+            1-1
+        </Button>    )
+}
+
+export default ShotElement
